57e2f59d2c99ea86861a5ee4a43cbed324a8b8eb
pbca26 <pbca26@gmail.com>
2017-10-23 15:39:09 +0300

    allow cors

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const	bodyParser = require('body-parser');
 var shepherd = require('./routes/shepherd');
 var app = express();
 
-/*guiapp.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', appConfig.dev ? '*' : 'http://127.0.0.1:3000');
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
 	res.header('Access-Control-Allow-Credentials', 'true');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
 	next();
-});*/
+});
 
 app.use(bodyParser.json({ limit: '1mb' })); // support json encoded bodies
 app.use(bodyParser.urlencoded({
