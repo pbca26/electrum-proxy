e0ae70a95eef2fb3462b487f5cb38a2bde2ebd34
pbca26 <pbca26@gmail.com>
2017-10-24 19:45:15 +0300

    block api

--- routes/electrum/block.js
+++ routes/electrum/block.js
@@ -1,9 +1,14 @@
 module.exports = (shepherd) => {
-  shepherd.get('/electrum/getblockinfo', (req, res, next) => {
-    shepherd.electrumGetBlockInfo(req.query.height, req.query.network)
+  shepherd.get('/getblockinfo', (req, res, next) => {
+    const ecl = new shepherd.electrumJSCore(req.query.port, req.query.ip, 'tcp');
+
+    ecl.connect();
+    ecl.blockchainBlockGetHeader(req.query.height)
     .then((json) => {
+      ecl.close();
+
       const successObj = {
-        msg: 'success',
+        msg: json.code ? 'error' : 'success',
         result: json,
       };
 
@@ -11,49 +16,22 @@ module.exports = (shepherd) => {
     });
   });
 
-  shepherd.electrumGetBlockInfo = (height, network) => {
-    return new shepherd.Promise((resolve, reject) => {
-      const ecl = new shepherd.electrumJSCore(shepherd.electrumServers[network].port, shepherd.electrumServers[network].address, shepherd.electrumServers[network].proto); // tcp or tls
+  shepherd.get('/getcurrentblock', (req, res, next) => {
+    const ecl = new shepherd.electrumJSCore(req.query.port, req.query.ip, 'tcp');
 
-      ecl.connect();
-      ecl.blockchainBlockGetHeader(height)
-      .then((json) => {
-        ecl.close();
-        shepherd.log('electrum getblockinfo ==>', true);
-        shepherd.log(json, true);
-
-        resolve(json);
-      });
-    });
-  }
-
-  shepherd.get('/electrum/getcurrentblock', (req, res, next) => {
-    shepherd.electrumGetCurrentBlock(req.query.network)
+    ecl.connect();
+    ecl.blockchainNumblocksSubscribe()
     .then((json) => {
+      ecl.close();
+
       const successObj = {
-        msg: 'success',
+        msg: json.code ? 'error' : 'success',
         result: json,
       };
 
       res.end(JSON.stringify(successObj));
     });
   });
-
-  shepherd.electrumGetCurrentBlock = (network) => {
-    return new shepherd.Promise((resolve, reject) => {
-      const ecl = new shepherd.electrumJSCore(shepherd.electrumServers[network].port, shepherd.electrumServers[network].address, shepherd.electrumServers[network].proto); // tcp or tls
-
-      ecl.connect();
-      ecl.blockchainNumblocksSubscribe()
-      .then((json) => {
-        ecl.close();
-        shepherd.log('electrum currentblock ==>', true);
-        shepherd.log(json, true);
-
-        resolve(json);
-      });
-    });
-  }
 
   return shepherd;
 };
