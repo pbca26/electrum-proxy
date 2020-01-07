791b15568dc5104585d73e7f89dd27ed48766a3f
pbca26 <pbca26@gmail.com>
2020-01-07 19:54:36 +0300

    init electrum conn manager loop

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,9 @@ api = require('./electrum/listunspent.js')(api);
 api = require('./electrum/estimate.js')(api);
 api = require('./electrum/merkle.js')(api);
 api = require('./electrum/server.js')(api);
+api = require('./electrum/conn.manager.js')(api);
+
+api.initElectrumManager();
 
 // default route
 api.get('/', (req, res, next) => {
